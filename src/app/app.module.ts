d1711de1daeecd866c6d1f5006aed65785afd7c1
lettertreslineas <marlon-380@hotmail.com>
2022-08-30 17:41:19 -0500

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ParteSuperiorComponent } from './parte-superior/parte-superior.component';
+import { ContenidoComponent } from './contenido/contenido.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, ParteSuperiorComponent ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([{path :'', component: ContenidoComponent},])],
+  declarations: [ AppComponent, ParteSuperiorComponent, ContenidoComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
