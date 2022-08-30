ccc76ffbec646e3e87bf080b6e5dae33effb2d3f
lettertreslineas <marlon-380@hotmail.com>
2022-08-30 17:15:10 -0500

    3 files modified, 2 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { ParteSuperiorComponent } from './parte-superior/parte-superior.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, ParteSuperiorComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/parte-superior/parte-superior.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parte-superior',
+  templateUrl: './parte-superior.component.html',
+  styleUrls: ['./parte-superior.component.css']
+})
+export class ParteSuperiorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
